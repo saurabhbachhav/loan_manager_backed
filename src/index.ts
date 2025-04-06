4d2425968b730fbc8d013551823d6c085ae4038e
saurabhbachhav <bachhavsaurabh7@gmail.com>
2025-04-07 03:26:25 +0530

    .

--- src/index.ts
+++ src/index.ts
@@ -17,9 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(
   cors({
-    origin:
-      "https://loan-manager-frontend-b8qtdijkp-saurabhbachhavs-projects.vercel.app",
-    credentials: true,
+    origin: "*",
   })
 );
 
